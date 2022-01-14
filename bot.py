5ed925ddb237aeaffb2784031dd82870f48349c7
vanceter231002 <68746683+vanceter231002@users.noreply.github.com>
2022-01-14 21:51:50 +0530

    New Commit

--- bot.py
+++ bot.py
@@ -16,31 +16,8 @@ bot=commands.Bot(command_prefix="!")
 async def on_ready():
     print(f'{bot.user.name} has connected to discord')
 
-#command created to give a random office quote
-@bot.command(name='office', help="responds with a random quote from the office")
-async def nine_nine(ctx):
-    office_quotes=[("Sometimes I’ll start a sentence and I don’t even know where it’s going."
-                   "I just hope I find it along the way."),
-                   ("I’m not superstitious,"
-                    "but I am a little stitious.."),
-                    ("I wish there was a way to know you’re"
-                     "in the good old days, before you’ve actually left them.")]
-    response=random.choice(office_quotes)
-    await ctx.send(response)
+
                                                         
-
-
-
-#command created to stimulate rolling of dice
-@bot.command(name="roll_dice", help="(!roll_dice {number_of_die} {number_of_sides}) to roll dice")
-
-async def roll_dice(ctx,number_of_dice: int ,number_of_sides: int):
-    response=""
-    for i in range(number_of_dice):
-        rand_int=random.choice(list(range(1,number_of_sides+1)))
-        response+=str(rand_int)+" "  
-    await ctx.send(response)
-    
 #a command to find horoscope for today based on zodiac sign
 @bot.command(name="horoscope",help="(!horoscope {Zodiac Sign}) for your horoscope of today")
 
@@ -61,6 +38,19 @@ async def horoscope(ctx,zodiac_sign):
             break
         except:
             pass
+
+
+#command created to stimulate rolling of dice
+@bot.command(name="roll_dice", help="(!roll_dice {number_of_die} {number_of_sides}) to roll dice")
+
+async def roll_dice(ctx,number_of_dice: int ,number_of_sides: int):
+    response=""
+    for i in range(number_of_dice):
+        rand_int=random.choice(list(range(1,number_of_sides+1)))
+        response+=str(rand_int)+" "  
+    await ctx.send(response)
+    
+
         
 #command created to give a list of top 10 movies sorted by popularity from imdb
 @bot.command(name="popular_movies",help="(!popular_movies) for listing the top 10 movies in imdb by popularity")
@@ -81,6 +71,20 @@ async def movies(ctx):
     final_string="`"+final_string+"`"
     await ctx.send(final_string)
     
+
+
+#command created to give a random office quote
+@bot.command(name='office', help="responds with a random quote from the office")
+async def office(ctx):
+    office_quotes=[("Sometimes I’ll start a sentence and I don’t even know where it’s going."
+                   "I just hope I find it along the way."),
+                   ("I’m not superstitious,"
+                    "but I am a little stitious.."),
+                    ("I wish there was a way to know you’re"
+                     "in the good old days, before you’ve actually left them.")]
+    response=random.choice(office_quotes)
+    await ctx.send(response)
+
 bot.run(TOKEN)
 
 
